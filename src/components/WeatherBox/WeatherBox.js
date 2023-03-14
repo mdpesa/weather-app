54e5303548734f6671b6ba3de2ed90b5c113053f
Marta Santos <mdpesa@gmail.com>
2023-03-14 16:03:58 +0100

    add UseState, useCallback and fetch with weatherData into the WeatherBox component, fix WeatherSummary component

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -1,15 +1,35 @@
 import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
+import { useCallback, useState } from 'react';
 
-const WeatherBox = props => {
+const WeatherBox = (props) => {
+  const [weather, setWeather] = useState('');
+  const handleCityChange = useCallback((city) => {
+    console.log('city:', city);
+
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0963d77f0f3a60fd604bcde9b5596630&units=metric`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        const weatherData = {
+          city: data.name,
+          temp: data.main.temp,
+          icon: data.weather[0].icon,
+          description: data.weather[0].main,
+        };
+        setWeather(weatherData);
+      });
+  }, []);
   return (
     <section>
-      <PickCity />
-      <WeatherSummary />
+      <PickCity action={handleCityChange} />
+      {weather && <WeatherSummary {...weather} />}
       <Loader />
     </section>
-  )
+  );
 };
 
 export default WeatherBox;
